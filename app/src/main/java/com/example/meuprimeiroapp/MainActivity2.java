3b5300c2326b2ab89d22a7fd0c2f5ade79194c40
mateuslph <mlptche@gmail.com>
2023-03-12 10:09:26 -0300

    Add Fragment A and Fragment B

--- app/src/main/java/com/example/meuprimeiroapp/MainActivity2.java
+++ app/src/main/java/com/example/meuprimeiroapp/MainActivity2.java
@@ -1,14 +1,49 @@
 package com.example.meuprimeiroapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        firstFragment = findViewById(R.id.BtnPrimeiroFragmento);
+        secondFragment = findViewById(R.id.BtnSegundoFragmento);
+
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment newFragment = new ExemploFragmento();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.flFragment, newFragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+            }
+        });
+
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment newFragment = new SegundoFragmento();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                transaction.replace(R.id.flFragment, newFragment);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+            }
+        });
+
     }
 }
